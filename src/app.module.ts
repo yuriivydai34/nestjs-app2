22e7af0b815a09b8ac5cce25f73b411d4e0051e3
Yurii Vydai <yuriivydai@gmail.com>
2025-08-08 00:44:04 +0300

    prisma posts, users

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UsersService } from './user.service';
+import { PostsService } from './post.service';
+import { PrismaService } from './prisma.service';
 
 @Module({
   imports: [],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, UsersService, PostsService, PrismaService],
 })
 export class AppModule {}
