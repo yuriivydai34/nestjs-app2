cb52a90ea73f06232782c8af9a0d3e73f8b4dc2d
Yurii Vydai <yuriivydai@gmail.com>
2025-08-12 13:43:06 +0300

    auth, bcrypt

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -2,6 +2,7 @@
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
 import { JwtService } from '@nestjs/jwt';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class AuthService {
@@ -15,7 +16,13 @@ export class AuthService {
     pass: string,
   ): Promise<{ access_token: string }> {
     const user = await this.usersService.findOne(username);
-    if (user?.password !== pass) {
+
+    if (!user || typeof user.password !== 'string') {
+      throw new UnauthorizedException();
+    }
+    const isMatch = await bcrypt.compare(pass, user.password);
+
+    if (!isMatch) {
       throw new UnauthorizedException();
     }
     const payload = { sub: user.id, username: user.username };
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -3,10 +3,11 @@ import { Injectable } from '@nestjs/common';
 import { User } from 'generated/prisma/client';
 import { PrismaService } from 'src/prisma.service';
 import CreateUserDto from './dto/create';
+import * as bcrypt from 'bcrypt';
 
 @Injectable()
 export class UsersService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   async findOne(username: string): Promise<User | undefined> {
     const user = await this.prisma.user.findUnique({
@@ -16,8 +17,14 @@ export class UsersService {
   }
 
   async create(data: CreateUserDto): Promise<User> {
+    const saltOrRounds = 10;
+    const password = data.password;
+    const hash = await bcrypt.hash(password, saltOrRounds);
     return this.prisma.user.create({
-      data,
+      data: {
+        ...data,
+        password: hash
+      }
     });
   }
 }
