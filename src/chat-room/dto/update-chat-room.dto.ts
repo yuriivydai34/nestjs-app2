a62746e63af40cbe485d74a9932917e694a93fba
Yurii Vydai <yuriivydai@gmail.com>
2025-09-29 22:02:42 +0300

    update room members

--- src/chat-room/dto/update-chat-room.dto.ts
+++ src/chat-room/dto/update-chat-room.dto.ts
@@ -1,4 +1,6 @@
 import { PartialType } from '@nestjs/swagger';
 import { CreateChatRoomDto } from './create-chat-room.dto';
 
-export class UpdateChatRoomDto extends PartialType(CreateChatRoomDto) {}
+export class UpdateChatRoomDto extends PartialType(CreateChatRoomDto) {
+    memberIds?: number[];
+}
